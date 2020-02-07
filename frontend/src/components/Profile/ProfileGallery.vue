400699c2c2652b531f15d9fb7f9bfb62466adee9
radley-carpio <60043552+radley-carpio@users.noreply.github.com>
2020-02-06 21:55:32 -0500

    Profile Page Demo
    Added demo profile page (not considering who is logged in).

---
+++ frontend/src/components/Profile/ProfileGallery.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container align="center">
+    <v-row
+      no-gutters
+      class="narrow">
+      <v-col
+        cols="12"
+        md="4"
+        v-for="item in 13"
+        :key="item.id">
+        <div class="gallery-item ma-3">
+          <v-img src="@/assets/account-placeholder.png" />
+          <div class="gallery-item-info">
+            <v-icon class="icon">
+              mdi-heart
+            </v-icon>
+            <span class="likes">100</span>
+            <v-icon class="icon">
+              mdi-chat
+            </v-icon>
+            <span>100</span>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default ({
+  name: 'ProfileGallery',
+  data() {
+    return {
+
+      gallery: [
+
+      ],
+    };
+  },
+});
+</script>
+
+<style>
+
+.gallery-item {
+    position: relative;
+    color: #fff;
+    cursor: pointer;
+}
+
+.gallery-item:hover .gallery-item-info,
+.gallery-item:focus .gallery-item-info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
+}
+
+.gallery-item-info {
+    display: none;
+    font-size: 1.2rem;
+    font-weight: 600;
+}
+
+.likes {
+    margin-right: 2.0rem;
+}
+
+.container {
+  position: relative;
+}
+
+.icon {
+    -webkit-text-fill-color: #fff;
+    margin-right: 0.3rem;
+    transform: scaleX(-1);
+    -moz-transform: scaleX(-1);
+    -webkit-transform: scaleX(-1);
+    -ms-transform: scaleX(-1);
+}
+</style>
---
+++ frontend/src/components/Profile/ProfileInfo.vue
@@ -0,0 +1,111 @@
+<template>
+  <v-container
+    class="my-12">
+    <v-layout row>
+      <v-flex
+        offset-sm1
+        sm4>
+        <div
+          align="center">
+          <v-avatar
+            size="150">
+            <v-img src="@/assets/account-placeholder.png" />
+          </v-avatar>
+        </div>
+      </v-flex>
+      <v-flex>
+        <v-layout
+          column>
+          <v-flex>
+            <span class="profile-user-name"> {{ username }}</span>
+            <v-btn
+              depressed="true"
+              class="profile-edit-btn mb-2">
+              <span>Edit Profile</span>
+            </v-btn>
+          </v-flex>
+          <v-flex class="mt-2 mb-5">
+            <span class="profile-stat-name">{{ postcount }}</span>
+            <span class="profile-stat-count"> posts</span>
+            <span class="profile-stat-name">{{ followercount }}</span>
+            <span class="profile-stat-count"> followers</span>
+            <span class="profile-stat-name">{{ followingcount }}</span>
+            <span class="profile-stat-count"> following</span>
+          </v-flex>
+          <v-flex>
+            <span class="profile-real-name">{{ realname }} </span>
+          </v-flex>
+          <v-flex>
+            <span>{{ bio }} </span>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+export default ({
+  name: 'ProfileInfo',
+  data() {
+    return {
+
+      displaypicture: '@/assets/picture.jpg',
+      username: 'radleycarpio',
+      postcount: '3',
+      followercount: '300',
+      followingcount: '300',
+      realname: 'Radley Carpio',
+      bio: 'This is the bio',
+    };
+  },
+});
+
+</script>
+
+<style scoped>
+
+  .profile-user-settings,
+  .profile-stats,
+  .profile-bio {
+      float: left;
+      width: calc(66.666% - 2rem);
+  }
+
+  .profile-user-name {
+      font-size: 1.75rem;
+      font-weight: 400;
+  }
+
+  .profile-edit-btn {
+      font-family: "Arial Narrow";
+      font-size: 0.70rem;
+      border: 0.05rem solid #dbdbdb;
+      border-radius: 0.3rem;
+      margin-left: 2rem;
+  }
+
+  .profile-stats {
+      font-size: 1rem;
+  }
+
+  .profile-bio {
+      font-size: 1rem;
+      font-weight: 400;
+  }
+
+  .profile-real-name,
+  .profile-stat-name,
+  .profile-edit-btn {
+      font-weight: 600;
+  }
+
+  .profile-stat-count
+  {
+      white-space: pre;
+      margin-right: 2.5rem;
+      font-weight: 400;
+  }
+
+</style>
