1824e54f7be909a19ba06eb516519f06182005ff
radley <radleycarpio@live.com>
2020-02-12 17:03:30 -0500

    Update ChangeDP.vue

--- frontend/src/components/EditProfile/ChangeDP.vue
+++ frontend/src/components/EditProfile/ChangeDP.vue
@@ -10,9 +10,8 @@
       @click="value = true">
       Change Profile Photo
     </v-btn>
-    <v-overlay :value="value">
+    <v-dialog width="400" v-model="value">
       <v-card
-        shaped
         light
         width="400"
         align="center">
@@ -24,13 +23,20 @@
           </v-flex>
           <v-divider width="100%" />
           <v-flex>
+            <input
+              type="file"
+              ref="fileInput"
+              accept="image/*"
+              style="display: none;"
+              @change="upload">
             <v-btn
               small
               depressed
               class="my-2"
               width="100%"
               color="white"
-              style="color: #3399ff">
+              style="color: #3399ff"
+              @click="$refs.fileInput.click()">
               Upload Photo
             </v-btn>
           </v-flex>
@@ -42,7 +48,8 @@
               class="my-2"
               width="100%"
               color="white"
-              style="color: #ff3333;">
+              style="color: #ff3333;"
+              @click="remove">
               Remove Current Photo
             </v-btn>
           </v-flex>
@@ -60,17 +67,42 @@
           </v-flex>
         </v-col>
       </v-card>
-    </v-overlay>
+    </v-dialog>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default ({
   name: 'ChangeDP',
   data() {
     return {
       value: false,
+      progress: false,
+      file: null,
+      data: null,
     };
+  },
+
+  watch: {
+    file() {
+      this.data = URL.createObjectURL(this.file);
+    },
+  },
+
+  methods: {
+    ...mapActions('posts', ['addPost']),
+
+    remove() {
+      console.log('Removed picture');
+      this.value = false;
+    },
+
+    upload() {
+      console.log('Uploaded picture');
+      this.value = false;
+    },
   },
 });
 </script>
