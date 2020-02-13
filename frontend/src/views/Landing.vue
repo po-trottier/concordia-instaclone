ec9259e6ba997b444671ef2392df17bd5fd928eb
Pierre-Olivier Trottier <po.trottier@gmail.com>
2020-02-12 22:32:52 -0500

    Fixed Login

--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -1,49 +1,95 @@
 <template>
-  <div>
-    <h2>Insta<span>Clone</span></h2>
+  <v-sheet
+    style="height: 100vh; width: 100vw;"
+    color="light"
+    tile>
     <v-container>
-      <v-input-field v-model="user" /><br>
-      <v-input-field v-model="pass" /><br>
-
-      <v-btn :to="{name: 'feed'} " text small>
-        Log in
-      </v-btn>
-
-      <v-btn :to="{name: 'signup' }" text small>
-        sign up
-      </v-btn>
-
+      <v-card
+        elevation="6"
+        class="pa-6 mx-auto my-12"
+        style="max-width: 500px;">
+        <v-card-title>
+          <h1 class="mb-6">
+            Welcome to Instaclone
+          </h1>
+        </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="email"
+            class="mb-6"
+            outlined
+            rounded
+            hide-details
+            type="email"
+            placeholder="E-mail" />
+          <v-text-field
+            v-model="password"
+            outlined
+            rounded
+            hide-details
+            type="password"
+            placeholder="Password" />
+        </v-card-text>
+        <v-card-actions>
+          <v-col class="text-center">
+            <v-btn
+              large
+              block
+              depressed
+              rounded
+              @click="login"
+              :loading="progress"
+              :disabled="!valid"
+              color="primary"
+              class="mb-4">
+              Sign In
+            </v-btn>
+            <v-btn
+              large
+              block
+              text
+              rounded
+              :to="{ name: 'signup' }">
+              Create an Account
+            </v-btn>
+          </v-col>
+        </v-card-actions>
+      </v-card>
     </v-container>
-  </div>
+  </v-sheet>
 </template>
 
 <script>
-  // eslint-disable-next-line no-unused-expressions
-  this.$firebase;
+export default {
+  name: 'Landing',
+  data() {
+    return {
+      email: null,
+      password: null,
+      progress: false,
+    };
+  },
 
-  export default {
-    name: 'Landing',
-    data() {
-      return {
-        email: null,
-        password: null,
-      };
+  computed: {
+    valid() {
+      return this.email
+        && this.password
+        && this.email.trim().length > 0
+        && this.password.trim().length > 0;
     },
-    methods: {
-      signup() {
-        this.$firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
-          .then((auth) => {
-            alert('Welcome to insta clone'); this.$router.replace({ name: 'SignUp' });
-          });
-      },
+  },
 
-      login() {
-        this.$firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-          .then((auth) => {
-            alert('You are signed in'); this.$router.replace({ name: 'Feed' });
-          });
-      },
+  methods: {
+    login() {
+      this.progress = true;
+      this.$firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+        .then(({ user }) => {
+          this.progress = false;
+          console.log(user.uid);
+          this.$router.replace({ name: 'feed' });
+        });
     },
-  };
+  },
+};
 
 </script>
--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,31 +1,26 @@
 <template>
-  <div>
-    <h2>Insta<span>Clone</span></h2>
-    <v-container>
-      <v-input-field v-model="email" /><br>
-      <v-input-field v-model="password" /><br>
-      <v-input-field v-model="name" /><br>
-      <v-input-field v-model="age" /><br>
-      <v-btn :to="{name: 'Landing'}" text small>Sign up </v-btn>
-    </v-container>
-  </div>
+  <h1>Sign Up</h1>
 </template>
 
 <script>
-  export default {
-    name: 'SignUp',
-    data() {
-      return {
-        email: null,
-        password: null,
-        name: null,
-        age: null,
-      };
+export default {
+  name: 'SignUp',
+
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+
+  methods: {
+    signup() {
+      this.$firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        .then((auth) => {
+          console.log(auth);
+          this.$router.replace({ name: 'feed' });
+        });
     },
-  };
+  },
+};
 </script>
-
-<style scoped>
-
-  
-</style>
